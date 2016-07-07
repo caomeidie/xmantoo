3ad39810597d38d283507dd6d9b0d8a550357315
Henry Lee <outshadow@sina.com>
2016-07-07 22:01:34 +0800

    split backend and frontend

---
+++ Application/Admin/Controller/BackendController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class BackendController extends Controller {
+    public function index(){
+        $this->show('aaaaaaaa');
+    }
+}
---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Admin\Controller;
+use Admin\Controller\BackendController;
+class IndexController extends BackendController {
+    public function index(){
+        $this->show('aaaaaaaa');
+    }
+}
